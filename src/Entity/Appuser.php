61ad05e01fba5f720273a8e301b236b4157adb6f
Jeanne-Louise GAUDIN <gaudin.jeannelouise@gmail.com>
2021-07-22 12:42:02 +0200

    import ChoiceType in Appusertype

--- src/Entity/Appuser.php
+++ src/Entity/Appuser.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+
 /**
  * @ORM\Entity(repositoryClass=AppuserRepository::class)
  */
