b0c721bff45958b907ed6d6d623d0a10091b40fb
Nicolas Gandon <nicolasgandon15@gmail.com>
2021-08-13 11:53:58 +0200

    Version 1

--- src/Form/AppuserType.php
+++ src/Form/AppuserType.php
@@ -6,6 +6,7 @@ use App\Entity\Appuser;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,15 +15,28 @@ class AppuserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username',EmailType::class)
+            ->add('username',EmailType::class, [
+                'attr' => [
+                    'placeholder' => 'email',
+                    'class' => 'form-control form-control-lg mb-3'
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'attr' => [
+                    'placeholder' => 'Mot de passe',
+                    'class' => 'form-control form-control-lg mb-3'
+                ]
+            ])
             ->add('roles' ,ChoiceType::class, [
                 'choices' => [
                     'Administrateur' => 'ROLE_ADMIN',
                     'Contributeur' => 'ROLE_CONTRIBUTOR'
                 ],
-                'multiple' => true
+                'multiple' => true,
+                'attr' => [
+                    'class' => 'form-control form-control-lg mb-3'
+                ]
             ])
-            ->add('password')
         ;
     }
 
--- src/Form/ContextType.php
+++ src/Form/ContextType.php
@@ -2,30 +2,30 @@
 
 namespace App\Form;
 
-use App\Entity\Univers;
+use App\Entity\Context;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UniversType extends AbstractType
+class ContextType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('label', TextType::class, [
-            'attr' => [
-                'placeholder' => 'Nom du label',
-                'class' => 'form-control form-control-lg mb-3'
-            ]
-        ])
+            ->add('label', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Nom du context',
+                    'class' => 'form-control form-control-lg mb-3'
+                ]
+            ])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Univers::class,
+            'data_class' => Context::class,
         ]);
     }
 }
