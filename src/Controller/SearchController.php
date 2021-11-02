956743c2887c0da006978a15c5685646decf1e35
Nicolas Gandon <ngandon@umanit.fr>
2021-11-02 14:07:03 +0100

    add commentaire

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -24,12 +24,9 @@ class SearchController extends AbstractController
         // Voir la doc de requestStack /!\
         $form = $this->createForm(SearchType::class);
         $form->handleRequest($requestStack->getMainRequest());
-        dump($this->resultats);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $this->resultats = $this->search($recherche);
-
-            dump($this->resultats);
-            // return $this->redirectToRoute('post');
         }
         return $this->render('search/index.html.twig', [
             'form' => $form->createView(),
@@ -37,7 +34,7 @@ class SearchController extends AbstractController
     }
 
 
-
+    // établie une connection avec la base de donnée
     public function connect()
     {
         if ($this->connexion === null) {
@@ -48,7 +45,7 @@ class SearchController extends AbstractController
         return $this->connexion;
     }
 
-
+    // recherche le mot clé en BDD et renvoie un tableau
     public function search($recherche)
     {
         $connexion = $this->connect();
@@ -58,6 +55,7 @@ class SearchController extends AbstractController
         return $resultats;
     }
 
+    // affiche le résultat de la recherche
     public function resultat() {
 
         return $this->render('search/result.html.twig', [
